4c5b3606c8c9b4be6261bc47d6c77311d74f6bbd
Gabriel <Gabriel@10.0.0.55>
2018-01-23 13:15:23 -0600

    Added Functionality:
    Reader.java will now read the test_data.json file
    
    There is a test included in the test class ClinicalTrialTests.java which
    will print all the readings to the console

---
+++ ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/Reader.java
@@ -0,0 +1,107 @@
+package edu.metrostate.ics372.thatgroup.clinicaltrial;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
+public class Reader {
+
+	File file;
+	FileReader in;
+	JsonReader reader;
+	List<Reading> readings;
+
+	enum Identifiers {
+		ARRAY_NAME("patient_readings"), PATIENT_ID("patient_id"), READING_TYPE("reading_type"), READING_ID(
+				"reading_id"), READING_VALUE("reading_value"), READING_DATE("reading_date");
+
+		private String identifier;
+
+		private Identifiers(String identifier) {
+			this.identifier = identifier;
+		}
+
+		public String get() {
+			return identifier;
+		}
+	}
+
+	public Reader() {
+
+	}
+	
+	public List<Reading> getReadings() {
+		return readings;
+	}
+
+	public void read(String filePath) throws IOException {
+		file = new File(filePath);
+		try {
+			in = new FileReader(file);
+			reader = new JsonReader(in);
+			while (reader.hasNext()) {
+				JsonToken nextToken = reader.peek();
+				if (JsonToken.BEGIN_OBJECT.equals(nextToken)) {
+					reader.beginObject();
+				} else if (JsonToken.NAME.equals(nextToken) && reader.nextName().equals(Identifiers.ARRAY_NAME.get())) {
+					reader.beginArray();
+					readArray(reader);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		reader.close();
+	}
+
+	private void readArray(JsonReader reader) throws IOException {
+		readings = new ArrayList<>();
+		String patientID = "";
+		String readingType = "";
+		String readingID = "";
+		String readingValue = "";
+		long readingDate = 0;
+		while (reader.hasNext()) {
+			reader.beginObject();
+			if (reader.nextName().equals(Identifiers.PATIENT_ID.get())) {
+				patientID = reader.nextString();
+			}
+			if (reader.nextName().equals(Identifiers.READING_TYPE.get())) {
+				readingType = reader.nextString();
+			}
+			if (reader.nextName().equals(Identifiers.READING_ID.get())) {
+				readingID = reader.nextString();
+			}
+			if (reader.nextName().equals(Identifiers.READING_VALUE.get())) {
+				readingValue = reader.nextString();
+			}
+			if (reader.nextName().equals(Identifiers.READING_DATE.get())) {
+				readingDate = reader.nextLong();
+			}
+			reader.endObject();
+			Reading reading = createReading(patientID, readingType, readingID, readingValue, readingDate);
+			readings.add(reading);
+		}
+	}
+
+	private Reading createReading(String patientID, String readingType, String readingID, String readingValue,
+			long readingDate) {
+
+		ReadingType readType = null;
+		LocalDateTime date = Instant.ofEpochMilli(readingDate).atZone(ZoneId.systemDefault()).toLocalDateTime();
+		for (ReadingType type : ReadingType.values()) {
+			if (type.get().equals(readingType)) {
+				readType = type;
+			}
+		}
+		return new Reading(patientID, readType, readingID, readingValue, date);
+	}
+}
--- ClinicalTrialClient/src/test/java/edu/metrostate/ics372/thatgroup/clinicaltrial/tests/ClinicalTrialTests.java
+++ ClinicalTrialClient/src/test/java/edu/metrostate/ics372/thatgroup/clinicaltrial/tests/ClinicalTrialTests.java
@@ -2,8 +2,13 @@ package edu.metrostate.ics372.thatgroup.clinicaltrial.tests;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.Test;
 
+import edu.metrostate.ics372.thatgroup.clinicaltrial.Reader;
+import edu.metrostate.ics372.thatgroup.clinicaltrial.Reading;
 import edu.metrostate.ics372.thatgroup.clinicaltrial.ReadingType;
 
 public class ClinicalTrialTests {
@@ -14,5 +19,20 @@ public class ClinicalTrialTests {
 		assertEquals("steps", ReadingType.STEPS.get());
 		assertEquals("temp", ReadingType.TEMP.get());
 		assertEquals("blood_press", ReadingType.BLOOD_PRESS.get());
+	}
+	
+	@Test
+	public void testReader() {
+		Reader rr = new Reader();
+		try {
+			rr.read("./data/test_data.json");
+			List<Reading> readings = rr.getReadings();
+			for (Reading reading : readings) {
+				System.out.println(reading);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
 	}
 }
