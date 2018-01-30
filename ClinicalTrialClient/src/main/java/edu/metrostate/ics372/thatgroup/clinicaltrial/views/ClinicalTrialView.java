b65eb7f123368f37b865bfa4bf56bca0ee58c18e
Gabriel Wallace <gabriel.wallace.dev@gmail.com>
2018-01-30 14:10:58 -0600

    GUI Refactoring & Additional Functionality:
    Can Now add patients to the trial and add readings for the patients.
    Blood pressure reading types are still in the works.

--- ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/ClinicalTrialView.java
+++ ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/ClinicalTrialView.java
@@ -1,11 +1,16 @@
 package edu.metrostate.ics372.thatgroup.clinicaltrial.views;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import edu.metrostate.ics372.thatgroup.clinicaltrial.patient.Patient;
 import edu.metrostate.ics372.thatgroup.clinicaltrial.reading.Reading;
+import edu.metrostate.ics372.thatgroup.clinicaltrial.reading.ReadingFactory;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -13,11 +18,14 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-
-
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class ClinicalTrialView implements Initializable {
+	Patient currentPatient;
 	ClinicalTrialViewModel viewModel;
 
 	@FXML
@@ -27,17 +35,39 @@ public class ClinicalTrialView implements Initializable {
 	@FXML
 	Menu menuHelp;
 	
+	@FXML
+	VBox inputForm;
+	@FXML
+	TextField addPatientIDTxt;
+	@FXML
+	DatePicker newPatientStartDate;
+	@FXML
+	Button addPatientBtn;
+
+	@FXML
+	StackPane editStackPane;
 	// Patient and Reading ListViews
 	@FXML
 	ListView<Patient> patientList;
 	@FXML
-	ListView<Reading> readingList;
-	
-	// Labels
-	
-	
+	TableView<Reading> readingTable;
+
+	@FXML
+	TableColumn<Reading, String> dateTimeCol;
+	@FXML
+	TableColumn<Reading, String> readingTypeCol;
+	@FXML
+	TableColumn<Reading, String> valueCol;
+	@FXML
+	TableColumn<Reading, String> readingIDCol;
 	// Actions Section
-	//Add Reading Form
+	@FXML
+	Button addReadingBtn;
+	@FXML
+	Button editReadingBtn;
+	@FXML
+	Button deleteReadingBtn;
+	// Input Form Add/Edit/Delete
 	@FXML
 	TextField patientIDTxt;
 	@FXML
@@ -47,47 +77,156 @@ public class ClinicalTrialView implements Initializable {
 	@FXML
 	TextField readingValueTxt;
 	@FXML
-	DatePicker readingDateTxt;
+	StackPane bloodPressStack;
+	@FXML
+	TextField systolic;
+	@FXML
+	TextField diastolic;
+	@FXML
+	DatePicker readingDate;
 	@FXML
 	Button submitBtn;
-	
-		// Edit Reading
-	@FXML
-	TextField patientIDTxtEdit;
-	@FXML
-	ChoiceBox<String> readingTypeChoiceEdit;
-	@FXML
-	TextField readingIDTxtEdit;
-	@FXML
-	TextField readingValueTxtEdit;
-	@FXML
-	DatePicker readingDateTxtEdit;
-	@FXML
-	Button submitBtnEdit;
-	
 
-	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		// Initialize the DatePicker to the current date for the Add Patient Form
+		newPatientStartDate.setValue(LocalDate.now());
+		// Hide the Add/Edit/Delete input Form until the user clicks the Add/Edit/Delete
+		// button
+		inputForm.setVisible(false);
+		// Initialize the view Model
 		viewModel = new ClinicalTrialViewModel();
-		patientList.setItems(viewModel.getPatients());
-		readingTypeChoice.setItems(viewModel.getTypes());
+		// Add patients to the patient list if there are any already existing in the
+		// trial
+		patientList.setItems(viewModel.getObservablePatients());
+		// Initialize the reading type choice box
+		readingTypeChoice.setItems(viewModel.getReadingTypeChoices());
+		// Set the reading type choice box default value
 		readingTypeChoice.getSelectionModel().selectFirst();
 
+		// Creates a change listener for the patient list
 		patientList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Patient>() {
 			@Override
-			public void changed(ObservableValue<? extends Patient> observable, Patient oldPatient, Patient newPatient) {			
-				patientIDTxt.setText(observable.getValue().toString());
-				readingList.getItems().clear();
-				readingList.setItems(viewModel.getPatientJournal(newPatient));
+			public void changed(ObservableValue<? extends Patient> observable, Patient oldPatient, Patient newPatient) {
+				/*
+				 * Sets the view model's currently observable patient journal to the one
+				 * currently selected in the patient list
+				 */
+				if (newPatient != null) {
+					viewModel.setObservablePatientJournal(newPatient);
+				}
+				/*
+				 * When a new patient is selected from the list, set currentPatient to the
+				 * currently selected patient to be used with fillReading() as it is used to
+				 * find the proper patient journal to display in readings table
+				 */
+				if (patientList.getSelectionModel().getSelectedItem() != null) {
+					currentPatient = patientList.getSelectionModel().getSelectedItem();
+					patientIDTxt.setText(currentPatient.getId());
+				}
+				/*
+				 * Fill the reading table with the proper patient journal of the currently
+				 * selected patient
+				 */
+				fillReadingTable();
 			}
 		});
-		
-		readingList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Reading>() {
+
+		/*
+		 * Sets the action listener for addPatientButton which will send the information
+		 * from the add new patient form to the model view to create and add a new
+		 * patient to the trials list
+		 */
+		addPatientBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
-			public void changed(ObservableValue<? extends Reading> observable, Reading oldReading, Reading newReading) {
-				// TODO Auto-generated method stub
+			public void handle(ActionEvent event) {
+				LocalDate startDate = newPatientStartDate.getValue();
+				viewModel.addPatientToTrial(addPatientIDTxt.getText(), startDate);
+				patientList.setItems(viewModel.getObservablePatients());
 			}
 		});
+
+		/*
+		 * Sets the action listener for the addReadingBtn which will show the add
+		 * reading input form and also sets the patientIDTxt field to the currently
+		 * selected patients ID
+		 */
+		addReadingBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				inputForm.setVisible(true);
+				submitBtn.setText("Add Reading");
+				patientIDTxt.setText(patientList.getSelectionModel().getSelectedItem().getId());
+			}
+		});
+
+		/*
+		 * Not yet implemented TODO
+		 */
+		editReadingBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+
+			}
+		});
+
+		/*
+		 * Not yet implemented TODO
+		 */
+		deleteReadingBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+			}
+		});
+
+		/*
+		 * Sets the change listener for the reading type choice box and changes the
+		 * input form to accommodate a blood pressure reading entry
+		 */
+		readingTypeChoice.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observableValue, String oldSelection,
+					String newSelection) {
+				if (newSelection.toLowerCase().equals("blood pressure")) {
+					bloodPressStack.toFront();
+					bloodPressStack.setVisible(true);
+					readingValueTxt.setVisible(false);
+				} else {
+					bloodPressStack.toBack();
+					bloodPressStack.setVisible(false);
+					readingValueTxt.setVisible(true);
+				}
+			}
+		});
+
+		/*
+		 * Sets the action listener for the input forms submitBtn. Currently only
+		 * supports the creation of a new reading TODO
+		 */
+		submitBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				viewModel.addReadingForPatient(patientList.getSelectionModel().getSelectedIndex(),
+						readingIDTxt.getText(), readingTypeChoice.getSelectionModel().getSelectedItem().toString(),
+						readingValueTxt.getText(), readingDate.getValue());
+				fillReadingTable();
+				patientList.getItems().clear();
+				patientList.setItems(viewModel.getObservablePatients());
+			}
+		});
+	}
+
+	/*
+	 * Fills the reading table columns with the proper information based on the
+	 * currently selected patient and their respective readings
+	 */
+	private void fillReadingTable() {
+		readingTable.setItems(viewModel.getPatientJournal(currentPatient));
+		readingIDCol.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getId().toString()));
+		readingTypeCol.setCellValueFactory(
+				cellData -> new ReadOnlyStringWrapper(ReadingFactory.getReadingType(cellData.getValue())));
+		valueCol.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getValue().toString()));
+		dateTimeCol
+				.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getDate().toString()));
 	}
 }
--- ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/ClinicalTrialViewModel.java
+++ ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/ClinicalTrialViewModel.java
@@ -1,70 +1,88 @@
 package edu.metrostate.ics372.thatgroup.clinicaltrial.views;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
-import edu.metrostate.ics372.thatgroup.clinicaltrial.JsonProcessor;
 import edu.metrostate.ics372.thatgroup.clinicaltrial.patient.Patient;
 import edu.metrostate.ics372.thatgroup.clinicaltrial.patient.PatientFactory;
 import edu.metrostate.ics372.thatgroup.clinicaltrial.Trial;
 import edu.metrostate.ics372.thatgroup.clinicaltrial.reading.Reading;
+import edu.metrostate.ics372.thatgroup.clinicaltrial.reading.ReadingFactory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class ClinicalTrialViewModel {
-	public static Trial trial;
-	ObservableList<Patient> patients;
-	ObservableList<Reading> readings;
-	ObservableList<String> types;
+	private Trial trial;
+	ObservableList<Patient> observablePatients;
+	ObservableList<Reading> observablePatientJournal;
+	ObservableList<String> readingTypeChoices = FXCollections.observableArrayList("Weight", "Steps", "Temp", "Blood Pressure");
 
 	public ClinicalTrialViewModel() {
-		// This is just a mock implementation.  TODO
-		types = FXCollections.observableArrayList("Weight", "Steps", "Temp", "Blood Pressure");
-		trial = new Trial();
-		patients = FXCollections.observableArrayList(trial.getPatientList());
-		readings = FXCollections.observableArrayList(trial.getPatientList().get(0).getJournal());
+		trial = new Trial("t01");
 	}
 
 	/**
-	 * @return the obvservableSimPatients
+	 * @return the trial
 	 */
-	public ObservableList<Patient> getPatients() {
-		return patients;
+	public Trial getTrial() {
+		return trial;
+	}
+
+
+	/**
+	 * @return the observablePatients
+	 */
+	public ObservableList<Patient> getObservablePatients() {
+		return FXCollections.observableArrayList(trial.getPatientList());
+	}
+
+
+	/**
+	 * @return the observablePatientJournal
+	 */
+	public ObservableList<Reading> getObservablePatientJournal() {
+		return observablePatientJournal;
 	}
 
 	/**
-	 * @param obvservableSimPatients the obvservableSimPatients to set
+	 * @param observablePatientJournal
+	 *            the observablePatientJournal to set
 	 */
-	public void setPatients(ObservableList<Patient> obvservableSimPatients) {
-		this.patients = obvservableSimPatients;
+	public void setObservablePatientJournal(Patient patient) {
+		observablePatientJournal = FXCollections.observableArrayList(patient.getJournal());
 	}
 
-	/**
-	 * @return the obvservableSimReadings
-	 */
-	public ObservableList<Reading> getReadings() {
-		return readings;
-	}
-
-	/**
-	 * @param obvservableSimReadings the obvservableSimReadings to set
-	 */
-	public void setReadings(ObservableList<Reading> obvservableSimReadings) {
-		this.readings = obvservableSimReadings;
-	}
-	
 	/**
 	 * @return the readingTypeChoices
 	 */
-	public ObservableList<String> getTypes() {
-		return types;
+	public ObservableList<String> getReadingTypeChoices() {
+		return readingTypeChoices;
 	}
+
 
 	public ObservableList<Reading> getPatientJournal(Patient patient) {
 		ObservableList<Reading> observablePatientJournal = FXCollections.observableArrayList(patient.getJournal());
 		return observablePatientJournal;
+	}
+
+	public void addPatientToTrial(String patientID, LocalDate startDate) {
+		Set<Reading> journal = new HashSet<>();
+		Patient patient = PatientFactory.getPatient("clinical");
+		patient.setId(patientID);
+		patient.setTrialStartDate(startDate);
+		patient.setJournal(journal);
+		trial.addPatient(patient);
+	}
+
+	public void addReadingForPatient(int patientIndex, String readingID, String readingType, String readingValue,
+			LocalDate localDate) {
+		LocalDateTime dateTime = localDate.atStartOfDay();
+		Reading reading = ReadingFactory.getReading(readingType);
+		reading.setPatientId(getObservablePatients().get(patientIndex).getId());
+		reading.setId(readingID);
+		reading.setValue(readingValue);
+		reading.setDate(dateTime);
+		trial.getPatientList().get(patientIndex).addReading(reading);
 	}
 }
