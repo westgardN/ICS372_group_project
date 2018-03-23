cb743932d4032c8761530121bee908558825e02e
Nico <nicolewestgard@192.168.99.108>
2018-03-23 12:52:20 -0500

    Issue #10 Finished Reading Test class. Cleaned up code.

--- ClinicalTrialClient/src/test/java/edu/metrostate/ics372/thatgroup/clinicaltrial/beans/ReadingTest.java
+++ ClinicalTrialClient/src/test/java/edu/metrostate/ics372/thatgroup/clinicaltrial/beans/ReadingTest.java
@@ -22,9 +22,10 @@ public class ReadingTest {
 	private static final String VALUE = "test";
 	private static final String CLINIC_ID = "test";
 	private static final String PATIENT_ID2 = "test2";
-	
+
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#hashCode()}.
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#hashCode()}.
 	 */
 	@Test
 	public final void testTwoReadingdThatAreEqualNotHaveEqualHashCodes() {
@@ -34,11 +35,10 @@ public class ReadingTest {
 		assertEquals(readingA, readingB);
 		assertEquals(readingA.hashCode(), readingB.hashCode());
 	}
-	
-	
 
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#equals(java.lang.Object)}.
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#equals(java.lang.Object)}.
 	 */
 	@Test
 	public final void testTwoReadingsThatAreNotEqualShouldNOtBeConsideredEqual() {
@@ -49,12 +49,13 @@ public class ReadingTest {
 	}
 
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#Reading()}.
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#Reading()}.
 	 */
 	@Test
 	public final void testDefaultNoArgsConstructedReading() {
 		Reading reading = new Reading();
-		
+
 		assertNotNull(reading);
 		assertNull(reading.getPatientId());
 		assertNull(reading.getId());
@@ -64,12 +65,13 @@ public class ReadingTest {
 	}
 
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#Reading(java.lang.String, java.lang.String, java.time.LocalDateTime, java.lang.Object, java.lang.String)}.
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#Reading(java.lang.String, java.lang.String, java.time.LocalDateTime, java.lang.Object, java.lang.String)}.
 	 */
 	@Test
 	public final void testConstructedReadingShouldNotBeNullStringStringLocalDateTimeObjectStringReading() {
 		Reading reading = new Reading(PATIENT_ID, ID, DATE, VALUE, CLINIC_ID);
-		
+
 		assertNotNull(reading);
 		assertEquals(PATIENT_ID, reading.getPatientId());
 		assertEquals(ID, reading.getId());
@@ -78,87 +80,90 @@ public class ReadingTest {
 		assertEquals(CLINIC_ID, reading.getClinicId());
 	}
 
-
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#toString()}.
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#toString()}.
 	 */
 	@Test
 	public final void testReadingToString() {
 		Reading reading = new Reading(PATIENT_ID, ID, DATE, VALUE, CLINIC_ID);
 		String result = reading.toString();
 		DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
-		String expected = "Reading " + reading.getId() + " for patient " + reading.getPatientId() + " taken on " + reading.getDate().format(formatter) 
-		+ " taken at " + reading.getClinicId() + " has a value of " + reading.getValue();
-		
+		String expected = "Reading " + reading.getId() + " for patient " + reading.getPatientId() + " taken on "
+				+ reading.getDate().format(formatter) + " taken at " + reading.getClinicId() + " has a value of "
+				+ reading.getValue();
+
 		assertEquals(expected, result);
 	}
-	
-	
 
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getPatientId()} and
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getPatientId()}
+	 * and
 	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#setPatientId(java.lang.String)}..
 	 */
 	@Test
 	public final void testReadingPatientId() {
 		Reading reading = new Reading();
 		reading.setPatientId(PATIENT_ID);
-		
+
 		assertEquals(PATIENT_ID, reading.getPatientId());
 	}
 
-
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getId()} and
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getId()}
+	 * and
 	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#setId(java.lang.String)}..
 	 */
 	@Test
 	public final void testReadingId() {
-		Reading reading =new Reading();
+		Reading reading = new Reading();
 		reading.setId(ID);
-		
+
 		assertEquals(ID, reading.getId());
 	}
 
-
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getDate()} and
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getDate()}
+	 * and
 	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#setDate(java.time.LocalDateTime)}..
 	 */
 	@Test
 	public final void testReadingDate() {
-		Reading reading =new Reading();
+		Reading reading = new Reading();
 		reading.setDate(DATE);
-		
+
 		assertEquals(DATE, reading.getDate());
 	}
 
-
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getValue()} and
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getValue()}
+	 * and
 	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#setValue(java.lang.Object)}..
 	 */
 	@Test
 	public final void testReadingValue() {
-		Reading reading =new Reading();
+		Reading reading = new Reading();
 		reading.setValue(VALUE);
-		
+
 		assertEquals(VALUE, reading.getValue());
 	}
 
-
 	/**
-	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getClinicId()} and
+	 * Test method for
+	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#getClinicId()}
+	 * and
 	 * {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Reading#setClinicId(java.lang.String)}..
 	 */
 	@Test
 	public final void testReadingClinicId() {
-		Reading reading =new Reading();
+		Reading reading = new Reading();
 		reading.setClinicId(CLINIC_ID);
-		
+
 		assertEquals(CLINIC_ID, reading.getClinicId());
 	}
-
-	
 
 }
