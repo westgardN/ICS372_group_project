5f64c3fbedcd9b5192ad049ed3e347fa77043726
Gabriel <Gabriel@10.0.0.55>
2018-03-22 19:57:37 -0500

    Bug Fix - Issue #9
    Actually fixed the app to no longer accept future LocalDateTime's

--- ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/StringResource.java
+++ ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/StringResource.java
@@ -15,7 +15,7 @@ public class StringResource {
 	public static final String BP_JSON = "blood_press";
 	public static final String TEMP_VALUE = "temperature";
 	public static final String ERR_DATE_MSG = "Date can not be empty or a future date.";
-	public static final String ERR_TIME_MSG = "Invalid time detected.  Please check the time input fields for errors.";
+	public static final String ERR_TIME_MSG = "Invalid time detected.  Please check the time input fields for errors.  Future times will not be accepted.";
 	public static final String ERR_ID_MSG = "Invalid reading id detected.  Please check the reading id input fields for errors.";
 	public static final String ERR_BLOOD_PRESSURE_MSG = "Invalid blood pressure detected.  Please check the blood pressure input fields for errors.";
 	public static final String ERR_TEMP_MSG = "Invalid temperature detected.  Please check the reading value input field for errors.";
