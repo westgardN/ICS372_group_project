2fa25b67372159ecacdff01032986c6fe3f4e8bf
Vincent J. Palodichuk <vj.palodichuk@gmail.com>
2018-03-24 04:50:12 -0500

    Jazzed up the PopupNotification class. It now auto closes itself :-D

--- ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/PopupNotification.java
+++ ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/views/PopupNotification.java
@@ -3,6 +3,11 @@
  */
 package edu.metrostate.ics372.thatgroup.clinicaltrial.views;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -18,6 +23,8 @@ import javafx.stage.WindowEvent;
  */
 public class PopupNotification {
 	
+	private static final long TIME_DELAY = 3000;
+
 	private static Popup createPopup(final String message) {
 	    final Popup popup = new Popup();
 	    popup.setAutoFix(true);
@@ -43,11 +50,32 @@ public class PopupNotification {
 	 */
 	public static void showPopupMessage(final String message, final Scene scene) {
 	    final Popup popup = createPopup(message);
-	    popup.setOnShown(new EventHandler<WindowEvent>() {
+	    EventHandler<WindowEvent> handler = new EventHandler<WindowEvent>() {
+	    	private ScheduledExecutorService sch = Executors.newSingleThreadScheduledExecutor();
 	        @Override
 	        public void handle(WindowEvent e) {
+	        	if (WindowEvent.WINDOW_SHOWN == e.getEventType()) {
 	        		popup.centerOnScreen();
+	        	    sch.schedule(new Runnable() {
+	        	        public void run() {
+	        	        	Platform.runLater(new Runnable() {
+	        	        		public void run() {
+			        	            if (popup != null && popup.isShowing()) {
+			        	            	popup.hide();
+			        	            }
+	        	        		}
+	        	        	});
+	        	        }
+	        	    }, TIME_DELAY, TimeUnit.MILLISECONDS);
+	        	} else if (WindowEvent.WINDOW_HIDING == e.getEventType()) {
+	        		sch.shutdown();
+	        	}
 	        }
-	    });        
+	    };
+	    
+	    popup.setOnShown(handler);
+	    popup.setOnHiding(handler);
+	    
+
 	    popup.show(scene.getWindow());
 	}}
