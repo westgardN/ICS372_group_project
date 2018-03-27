80e2af989ad3d286f440c9c99e032fd42c250db6
Vincent J. Palodichuk <vj.palodichuk@gmail.com>
2018-03-27 00:05:24 -0500

    Huge refactor of the ReadingView and the ClinicsView and PatientsView.
    Removed the AddPatientView and AddClinicView and merged them in to
    PatientsView and ClinicsView respectively.
    
    Added the use of Executors for kicking off the import and export jobs.
    
    Added "clear" buttons to the Clinics and Patients views to clear the
    forms the current selection.
    
    Added the ability to update Clinic names.
    
    Things are working pretty good.

--- ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/resources/Strings.java
+++ ClinicalTrialClient/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/resources/Strings.java
@@ -9,6 +9,8 @@ package edu.metrostate.ics372.thatgroup.clinicaltrial.resources;
  * @author That Group
  */
 public class Strings {
+	public static final String ADD = "Add";
+	public static final String UPDATE = "Update" ;
 	public static final String LOGO_PATH = "/img/logov2_256x256.png";
 	public static final String CLINICAL_TRIAL_VIEW_FXML = "/fxml/ClinicalTrialView.fxml";
 	public static final String ADD_PATIENT_VIEW_FXML = "/fxml/AddPatientView.fxml";
@@ -29,13 +31,18 @@ public class Strings {
 	public static final String ERR_BLOOD_PRESSURE_MSG = "Invalid blood pressure detected.  Please check the blood pressure input fields for errors.";
 	public static final String ERR_TEMP_MSG = "Invalid temperature detected.  Please check the reading value input field for errors.";
 	public static final String ERR_VALUE_MSG = "Invalid input detected.  Please check the reading value input field for errors.";
+	public static final String ERR_RECEIVED_MSG = "Received error: ";
 	public static final String INACTIVE_PATIENT_MSG = "This patient is not active in the clinical trial.";
 	public static final String PATIENT_NOT_ADDED_MSG = "Unable to add patient to the trial.";
 	public static final String READING_NOT_ADDED_MSG  = "Unable to add the reading. Please verify the Reading ID is unique for this patient.";
-	public static final String CLINIC_NOT_ADDED_MSG = "Unable to add the clinic. Please verify that Clinic ID is unique";
+	public static final String READING_NOT_UPDATED_MSG  = "Unable to update the reading. Please verify the Reading ID is unique for this patient.";
+	public static final String CLINIC_NOT_ADDED_MSG = "Unable to add the clinic. Please verify that Clinic ID is unique.";
+	public static final String CLINIC_NOT_UPDATED_MSG = "Unable to update the clinic.";
 	public static final String PATIENT_ADDED_MSG = "New Patient Added";
 	public static final String READING_ADDED_MSG = "Reading has been added.";
+	public static final String READING_UPDATED_MSG = "Reading has been updated.";
 	public static final String CLINIC_ADDED_MSG = "Clinic has been added.";
+	public static final String CLINIC_UPDATED_MSG = "Clinic has been updated.";
 	public static final String SPECIAL_CHAR_MSG = "No special characters allowed.";
 	public static final String SELECT_PATIENT_TRIAL_DATE_TITLE_FMT = "Select Trial %s Date for Patient %s";
 	public static final String SELECT_PATIENT_TRIAL_DATE_LABEL_FMT = "Please select the date patient %s %s the trial";
