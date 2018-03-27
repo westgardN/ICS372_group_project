f37903301af5797bdb112936dc84aaa4a2e51873
Nico <nicolewestgard@192.168.99.108>
2018-03-27 12:01:13 -0500

    Issue #10 Added Junit tests for readings. Not yet complete, weightTest class needs review.

---
+++ ClinicalTrialClient/src/test/java/edu/metrostate/ics372/thatgroup/clinicaltrial/beans/TempTest.java
@@ -0,0 +1,56 @@
+package edu.metrostate.ics372.thatgroup.clinicaltrial.beans;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author nicolewestgard
+ *
+ */
+public class TempTest {
+
+	/**
+	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Temp#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Temp#getValue()}.
+	 */
+	@Test
+	public final void testGetValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Temp#setValue(java.lang.Object)}.
+	 */
+	@Test
+	public final void testSetValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Temp#Temp()}.
+	 */
+	@Test
+	public final void testTemp() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.metrostate.ics372.thatgroup.clinicaltrial.beans.Temp#Temp(java.lang.String, java.lang.String, java.time.LocalDateTime, java.lang.Object, java.lang.String)}.
+	 */
+	@Test
+	public final void testTempStringStringLocalDateTimeObjectString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
